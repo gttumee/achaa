34d478cc701532bb5a924afca51bef824dc4de68
tumee <tmkee0525@gmail.com>
2025-07-05 00:43:56 +0900

    new change

--- app/Filament/Resources/CustomerResource/Pages/EditCustomer.php
+++ app/Filament/Resources/CustomerResource/Pages/EditCustomer.php
@@ -9,6 +9,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditCustomer extends EditRecord
 {
     protected static string $resource = CustomerResource::class;
+        protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
 
     protected function getHeaderActions(): array
     {
