3e90e283632a7bb8551d36d1d8ab69440ed9dd8d
tumee <tmkee0525@gmail.com>
2025-07-05 08:32:25 +0900

    add to tab

--- app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
+++ app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCustomers extends ListRecords
 {
@@ -18,4 +22,19 @@ class ListCustomers extends ListRecords
 
         ];
     }
+public function getTabs(): array
+{
+    return [
+         'all' => Tab::make('Бүгд'),
+        'not_pay' => Tab::make('Төлөгдөөгүй')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('payment_status', 'not_payd'))
+             ->badge(Customer::query()->where('payment_status', 'not_payd')->count()),
+        'payd' => Tab::make('Төлсөн')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('payment_status', 'payd')),
+        'card' => Tab::make('Карт')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('payment_type', '1')),
+        'cash' => Tab::make('Бэлэн')
+            ->modifyQueryUsing(fn (Builder $query) => $query->where('payment_type', '0')),
+    ];
+}
 }
